b7c3f2710640d46ee0e7786426f8984fdf174038
bichthuan0510 <bichthuan854@gmail.com>
2025-08-09 15:54:34 +0700

    fix chrometest file

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -7,13 +7,21 @@ import org.testng.annotations.Test;
 public class ChromeTest {
 
     @Test
+    public void normalModeTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+
+    @Test
     public void headlessModeTest() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.selenium.dev/");
-        Assert.assertEquals(driver.getTitle(),"Selenium");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
         driver.quit();
     }
 }
