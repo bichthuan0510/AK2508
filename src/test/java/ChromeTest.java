93b8e7f982934d5c39e7074e65f2eccd48b88a6c
bichthuan0510 <bichthuan854@gmail.com>
2025-08-09 15:50:28 +0700

    chrometest01

---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+
+    @Test
+    public void headlessModeTest() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+}
