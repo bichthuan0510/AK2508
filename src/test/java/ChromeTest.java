2b6e9dcbdcf3b70bab906346b0d4f5de9d21733e
bichthuan0510 <bichthuan854@gmail.com>
2025-08-12 16:51:21 +0700

    commit lan 1

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v123.emulation.Emulation;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Optional;
 
 public class ChromeTest {
 
@@ -38,6 +44,28 @@ public class ChromeTest {
         Assert.assertEquals(driver.getTitle(), "Selenium");
 
         // Đóng trình duyệt
+        driver.quit();
+    }
+    @Test
+    void openBrowserWithFakeGeoLocation(){
+        WebDriver driver = new ChromeDriver();
+        DevTools devTools = ((HasDevTools) driver).getDevTools();
+        devTools.createSession();
+        // Mountain view
+        devTools.send(Emulation.setGeolocationOverride(
+                Optional.of(37.386052),
+                Optional.of(-122.083851),
+                Optional.of(1),
+                Optional.of(100),
+                Optional.of(1),
+                Optional.of(1),
+                Optional.of(0)
+        ));
+        driver.get("https://the-internet.herokuapp.com/geolocation");
+        driver.findElement(By.xpath("//button[.='Where am I?']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("#lat-value")).getText(),"37.386052");
+        Assert.assertEquals(driver.findElement(By.cssSelector("#long-value")).getText(),"-122.083851");
+
         driver.quit();
     }
 }
